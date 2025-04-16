5e909e91e5984aa140a63bed0c2900266b925f77
CrsSilva88 <crs_nofx@hotmail.com>
2025-04-16 16:14:50 -0300

    melhorando a organizaçào

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from interface import iniciar_interface
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
+
+from src.interface import iniciar_interface
 
 def main():
     iniciar_interface()
-
-
-
 
 if __name__ == "__main__":
     main()
